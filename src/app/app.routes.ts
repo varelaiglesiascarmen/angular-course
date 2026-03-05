97be46001a66ba51ecdd47e77dc4e43ebbcbcc6c
Carmen Varela <cvi0004@alu.medac.es>
2026-03-05 16:12:05 +0100

    task 50 done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './pages/counter.component/counter.component';
 import { HeroPageComponent } from './pages/hero-page.component/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
 
@@ -10,9 +11,11 @@ export const routes: Routes = [
   // hero route
   { path: 'hero', component: HeroPageComponent,},
 
+  // dragon ball route
+  { path: 'dragonball', component: DragonballPageComponent,},
+
   /*  wildcard route > this route will be used when the user tries
   to access a route that does not exist in the application, it will redirect
   the user to the default route. */
-  { path: '**', redirectTo: '',}
-
+  { path: '**', redirectTo: '',},
 ];
