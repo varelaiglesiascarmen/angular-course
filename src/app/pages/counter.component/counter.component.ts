f9b92557fd4803af8c782d3cfed03144ca9f9273
Carmen Varela <cvi0004@alu.medac.es>
2026-03-05 11:45:15 +0100

    task done

--- src/app/pages/counter.component/counter.component.ts
+++ src/app/pages/counter.component/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter.component',
@@ -10,14 +10,19 @@ export class CounterComponent {
 
   counter = 10;
 
-  // this function resets the counter to its initial state when clicked
-  resetCounter(){
-    this.counter = 10;
-  }
+  counterSignal = signal(10);
 
   // this function increases or decreases the counter by the value passed as an argument when clicked
   incraseBy(value: number){
     this.counter += value;
+    // this.counterSignal.set(this.counterSignal() + value); > this is the same as the line below but more concise
+    this.counterSignal.update( (current) => current + value);
+  }
+
+  // this function resets the counter to its initial state when clicked
+  resetCounter(){
+    this.counter = 0;
+    this.counterSignal.set(0);
   }
 
 }
