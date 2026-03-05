26340877798635ec13184f9f5f62322b4b853aff
Carmen Varela <cvi0004@alu.medac.es>
2026-03-05 16:26:31 +0100

    task 51 done

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball-page',
@@ -8,4 +14,9 @@ import { Component } from '@angular/core';
 })
 export class DragonballPageComponent {
 
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
+  ]);
 }
