70e38450c8d6c9493390bb7a5ea27f91e6a9dd9e
Carmen Varela <cvi0004@alu.medac.es>
2026-03-05 14:06:25 +0100

    task 47 done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,14 @@ import { HeroPageComponent } from './pages/hero-page.component/hero-page.compone
 export const routes: Routes = [
 
   // default route
- { path: '', component: CounterComponent,},
+  { path: '', component: CounterComponent,},
 
- // hero route
- { path: 'hero', component: HeroPageComponent,}
+  // hero route
+  { path: 'hero', component: HeroPageComponent,},
+
+  /*  wildcard route > this route will be used when the user tries
+  to access a route that does not exist in the application, it will redirect
+  the user to the default route. */
+  { path: '**', redirectTo: '',}
 
 ];
